2ecbb9b97bdc48c2aaac94871a9103e4e0970008
jimmy <xingjianjun1982@163.com>
2020-03-21 22:30:44 +0800

    update cases

--- com.jimmy.data.structure/src/com/jimmy/data/structure/tree/exam/Exam1.java
+++ com.jimmy.data.structure/src/com/jimmy/data/structure/tree/exam/Exam1.java
@@ -50,8 +50,7 @@ public class Exam1 {
 		}
 		System.out.print(max);	
 	}
-	
-	
+
 	public void question3_findNode() {
 		BinTree bt=new BinTree();
 		BinTreeNode root=bt.createBinTree(30);
@@ -287,11 +286,195 @@ public class Exam1 {
 		}
 		bt.preOrderTree(root);
 	}
+
 	
+	public void question14_countLeaveNode() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(30);
+		System.out.println();
+		ArrayQueue<BinTreeNode> aq=new ArrayQueue<BinTreeNode>(BinTreeNode.class);
+		aq.enqueue(root);
+		int leaveCon=0;
+		while(!aq.isEmpty()) {
+			BinTreeNode node=aq.dequeue();
+			if(node.getLeft()==null&&node.getRight()==null) {
+				leaveCon++;
+			}
+			if(node.getLeft()!=null) {
+				aq.enqueue(node.getLeft());
+			}
+			if(node.getRight()!=null) {
+				aq.enqueue(node.getRight());
+
+			}
+		}
+		System.out.print(leaveCon);
+
+	}
+	
+	public void question15_countFullNode() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(30);
+		System.out.println();
+		ArrayQueue<BinTreeNode> aq=new ArrayQueue<BinTreeNode>(BinTreeNode.class);
+		aq.enqueue(root);
+		int fullCon=0;
+		while(!aq.isEmpty()) {
+			BinTreeNode node=aq.dequeue();
+			if(node.getLeft()!=null&&node.getRight()!=null) {
+				fullCon++;
+			}
+			if(node.getLeft()!=null) {
+				aq.enqueue(node.getLeft());
+			}
+			if(node.getRight()!=null) {
+				aq.enqueue(node.getRight());
+
+			}
+		}
+		System.out.print(fullCon);
+	}
+	
+	public void question16_countHalfNode() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(30);
+		System.out.println();
+		ArrayQueue<BinTreeNode> aq=new ArrayQueue<BinTreeNode>(BinTreeNode.class);
+		aq.enqueue(root);
+		int fullCon=0;
+		while(!aq.isEmpty()) {
+			BinTreeNode node=aq.dequeue();
+			if(node.getLeft()!=null||node.getRight()!=null) {
+				fullCon++;
+			}
+			if(node.getLeft()!=null) {
+				aq.enqueue(node.getLeft());
+			}
+			if(node.getRight()!=null) {
+				aq.enqueue(node.getRight());
+
+			}
+		}
+		System.out.print(fullCon);
+	}
+	
+	public void question17_isTwoTreeSame() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(10);
+		System.out.println();
+		BinTreeNode root2=bt.clone(root);
+		//BinTreeNode root2=bt.createBinTree(11);
+		boolean isMatch=isTreeNodeIndentify(root, root2);
+		System.out.print(isMatch);
+
+	}
+	private boolean isTreeNodeIndentify(BinTreeNode source, BinTreeNode target) {
+		if(source==null&&target==null)return true;
+		if(source==null||target==null) return false;
+		if(source.getData()!=target.getData()) return false;
+		
+		return isTreeNodeIndentify(source.getLeft(),target.getLeft())&&isTreeNodeIndentify(source.getRight(),target.getRight());
+	}
+	
+	
+	public void question18_DiameterOfTree() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(10);
+		System.out.println();
+		Integer diamater=new Integer(0);
+		diameterOfTree(root,diamater);
+		System.out.println(diamater);
+	}
+	
+	public int diameterOfTree(BinTreeNode root,Integer diamater) {
+		if(root==null) return 0;
+		int left=diameterOfTree(root.getLeft(),diamater);
+		int right=diameterOfTree(root.getRight(),diamater);
+		if(left+right>diamater) diamater=left+right;
+		return Math.max(left, right)+1;
+	}
+	
+	
+	public void question19_MaxValueOfLevel() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(10);
+		System.out.println();
+		ArrayQueue<BinTreeNode> aq=new ArrayQueue<BinTreeNode>(BinTreeNode.class);
+		aq.enqueue(root);
+		aq.enqueue(null);
+		int max=0;
+		int levelValue=0;
+		int level=0;
+		int maxLevel=0;
+		while(!aq.isEmpty()) {
+			BinTreeNode node=aq.dequeue();
+			if(node==null) {
+				level++;
+				if(max<levelValue) {
+					max=levelValue;
+					maxLevel=level;
+				}
+				levelValue=0;
+				if(!aq.isEmpty()) {
+					aq.enqueue(null);
+				}
+				continue;
+			}
+			levelValue+=node.getData();
+			if(node.getLeft()!=null) {
+				aq.enqueue(node.getLeft());
+			}
+			if(node.getRight()!=null) {
+				aq.enqueue(node.getRight());
+			}
+		}
+		System.out.println("max value is "+ max+" and level is "+maxLevel);
+	}
+	
+	
+	
+	public void question22_SumTreeNode() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(4);
+		System.out.println();
+		int sum=sumOfTree(root);
+		
+		System.out.println(sum);
+	}
+	
+	private int sumOfTree(BinTreeNode root) {
+		if(root!=null) {
+			return root.getData()+sumOfTree(root.getLeft())+sumOfTree(root.getRight());
+		}else {
+			return 0;
+		}	
+	}
+	
+	public void question23_SumTreeNode() {
+		BinTree bt=new BinTree();
+		BinTreeNode root=bt.createBinTree(10);
+		System.out.println();
+		ArrayQueue<BinTreeNode> aq=new ArrayQueue<BinTreeNode>(BinTreeNode.class);
+		aq.enqueue(root);
+		int sum=0;
+		while(!aq.isEmpty()) {
+			BinTreeNode node=aq.dequeue();
+			sum+=node.getData();
+			if(node.getLeft()!=null) {
+				aq.enqueue(node.getLeft());
+			}
+			if(node.getRight()!=null) {
+				aq.enqueue(node.getRight());
+			}
+		}
+		
+		System.out.println(sum);
+		
+	}
 	
 	public static void main(String[] args) {
 		Exam1 ex=new Exam1();
-		ex.question13_deleteTreeNode();
+		ex.question23_SumTreeNode();
 	}
 
 }
