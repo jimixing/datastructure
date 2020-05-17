b40f0e390f84c42ffef9830c703cdfcd1486d670
jimmy <xingjianjun1982@163.com>
2020-05-17 11:01:52 +0800

    update case

--- com.jimmy.data.structure/src/com/jimmy/data/structure/dp/exam/Exam1.java
+++ com.jimmy.data.structure/src/com/jimmy/data/structure/dp/exam/Exam1.java
@@ -134,7 +134,7 @@ public class Exam1 {
 			M[0]=0;
 		}
 		for(int i=1;i<Len;i++) {
-			if(array[i]>=0) {
+			if(array[i]+M[i-1]>=0) {
 				M[i]=M[i-1]+array[i];
 			}else {
 				M[i]=0;
@@ -241,6 +241,50 @@ public class Exam1 {
 		
 	}
 	
+	private int[][] generateTwoArray(int m, int n){
+		int[][] array=new int[m][n];
+		
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				array[i][j]=(int)(Math.random()*50);
+			}
+		}
+		
+		for(int i=0;i<m;i++) {
+			System.out.println(Arrays.toString(array[i]));
+		}
+		return array;
+	}
+	
+	
+	
+	public void question40_shortestPath() {
+		int m=10,n=12;
+		int[][] array=generateTwoArray(m,n);
+		int[][] dp=new int[m][n];
+		dp[0][0]=array[0][0];
+		
+		for(int i=1;i<m;i++) {
+			dp[i][0]=array[i][0];
+		}
+		for(int j=1;j<n;j++) {
+			dp[0][j]=array[0][j];
+		}
+		for(int i=1;i<m;i++) {
+			for(int j=1;j<n;j++) {
+				dp[i][j]=Math.min(dp[i-1][j], dp[i][j-1])+array[i][j];
+			}
+		}
+		System.out.println(dp[m-1][n-1]);
+		
+		for(int i=0;i<m;i++) {
+			System.out.println(Arrays.toString(dp[i]));
+		}
+		
+	}
+	
+	
+	
 	public void question42_leastCount() {
 		int n=20;
 		int[] dp=new int[n+1];
@@ -294,10 +338,133 @@ public class Exam1 {
 		
 	}
 	
+	
+	public void question44_findchange() {
+		int[] coinKind= {1,2,5,10,20};
+		int change =54;
+		
+		int[] f=new int[change+1];
+		f[0]=0;
+		f[1]=1;
+		f[2]=1;
+		f[3]=f[2]+1;
+		f[4]=Math.min(f[3]+1, f[4-2]+1);
+		for(int i=4;i<=change;i++) {
+			int min=f[i-1]+1;
+			for(int j=0;j<coinKind.length;j++) {
+				if(i<coinKind[j]) {
+					continue;				
+				}else {
+					if(f[i-coinKind[j]]<min) {
+						min=f[i-coinKind[j]];
+					}
+				}
+			}
+			f[i]=min+1;
+
+		}
+		
+		System.out.println(f[change]);	
+	}
+	
+	public void question45_maxLenString() {
+		String str1="acbbsdef";
+		String str2="acbesdsd";
+		char[] ch1=str1.toCharArray();
+		char[] ch2=str2.toCharArray();
+		int[][] dp=new int[ch1.length][ch2.length];
+		for(int i=0;i<ch2.length;i++) {
+			if(ch1[0]==ch2[i]) {
+				dp[0][i]=1;
+			}
+		}
+		for(int j=0;j<ch1.length;j++) {
+			if(ch2[0]==ch1[j]) {
+				dp[j][0]=1;
+			}
+		}
+		
+		int max=0,ch1end=0;
+		for(int i=1;i<ch1.length;i++) {
+			for(int j=1;j<ch2.length;j++) {
+				if(ch1[i]==ch2[j]) {
+					dp[i][j]=dp[i-1][j-1]+1;
+					if(dp[i][j]>max) {
+						max=dp[i][j];
+						ch1end=i;
+					}
+				}
+				
+			}
+		}
+		System.out.println(max);
+		System.out.println(str1.substring(ch1end-max+1,ch1end+1));
+		
+	}
+	
+	public void question46_minCostStringEdit() {
+		String str1="adcmn";
+		String str2="abcn";
+		int add=5,del=3, mod=5+3;
+		int[][] dp=new int[str1.length()+1][str2.length()+1];
+		dp[0][0]=0;
+		for(int i=1;i<=str2.length();i++) {
+			dp[0][i]=add*i;
+		}
+		for(int i=1;i<=str1.length();i++) {
+			dp[i][0]=del*i;
+		}
+		for(int i=1;i<=str1.length();i++) {
+			for(int j=1;j<=str2.length();j++) {
+				if(str1.charAt(i-1)==str2.charAt(j-1)) {
+					int tem=dp[i][j-1]+add>dp[i-1][j]+del?dp[i-1][j]+del:dp[i][j-1]+add;
+					dp[i][j]=tem>dp[i-1][j-1]?dp[i-1][j-1]:tem;
+				}else {
+					int tem=dp[i][j-1]+add>dp[i-1][j]+del?dp[i-1][j]+del:dp[i][j-1]+add;
+					dp[i][j]=tem>dp[i-1][j-1]+mod?dp[i-1][j-1]+mod:tem;
+				}
+			}
+			
+		}
+		System.out.print(dp[str1.length()][str2.length()]);
+		
+	}
+	
+	
+	public void question47_maxUpSubValue() {
+		
+		int[] array = new int[]{1, 4, 2, 5, 3};
+		int[] dp=new int[array.length];
+		dp[0]=1;
+		
+		for(int i=1;i<array.length;i++) {
+			int max=0;
+			for(int j=0;j<i;j++) {
+				if(array[j]<array[i]) {
+					max=Math.max(dp[j],max);
+				}
+			}
+			dp[i]=max+1;
+		}
+		int max=0;
+		for(int i=0;i<dp.length;i++) {
+			if(max<dp[i]) {
+				max=dp[i];
+			}
+			
+		}
+		
+		System.out.print(max);
+		
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Exam1 ex=new Exam1();
-		ex.question42_leastCount();
+		ex.question47_maxUpSubValue();
 	}
 
 }
